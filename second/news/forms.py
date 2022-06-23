679b753611bdac7d6ca131324e59e3151981699a
SadFox16 <oleg.s.kerimov@yandex.com>
2022-06-23 22:02:29 +0300

    Починил форму добавления новости
    Исправлена ошибка, убрал ненужные запятые при описании формы

--- second/news/forms.py
+++ second/news/forms.py
@@ -4,8 +4,8 @@ from .models import Category
 
 
 class NewsForm(forms.Form): #класс для описания вида формы добавления новости
-    title = forms.CharField(max_length=150), #название
-    content = forms.CharField(), #текст
-    is_published = forms.BooleanField(), #опубликована ли
-    category = forms.ModelChoiceField(queryset=Category.objects.all()), #категория
+    title = forms.CharField(max_length=150, label='Заголовок:') #название
+    content = forms.CharField(label='Текст:') #текст
+    is_published = forms.BooleanField(label='Публикация:') #опубликована ли
+    category = forms.ModelChoiceField(label='Категория:', queryset=Category.objects.all()) #категория
 
