89a16338c36db2c76f12cf6c4dd2312cacad109f
SadFox16 <oleg.s.kerimov@yandex.com>
2022-06-24 21:36:44 +0300

    Авторизация и выход
    Сделана форма авторизации и выход из учетной записи.

--- second/news/forms.py
+++ second/news/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import News
 import re
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
@@ -16,6 +16,11 @@ class UserRegistrationForm(UserCreationForm): #форма регистрации
     class Meta:
         model = User
         fields = {'username', 'password1', 'password2', 'email'}
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя',widget=forms.TextInput(attrs={"class": "form-control"}))  # поле для имени
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={"class": "form-control"}))
 
 
 class NewsForm(forms.ModelForm): #класс для описания вида формы добавления новости
--- second/news/views.py
+++ second/news/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from .models import News, Category
-from .forms import NewsForm, UserRegistrationForm
+from .forms import NewsForm, UserRegistrationForm, UserLoginForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.contrib import messages
+from django.contrib.auth import login, logout
 
 
 #функция для регистрации
@@ -13,9 +14,10 @@ def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             messages.success(request, 'Вы успешно зарегистрировались!')
-            return redirect('login')
+            return redirect('home')
         else:
             messages.error(request, 'Ошибка регистрации')
     else:
@@ -24,9 +26,22 @@ def register(request):
 
 
 #функция для авторизации
-def login(request):
-    return render(request, 'news/login.html')
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {"form": form})
 
+
+#функция для выхода
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 #классы для страниц
 class HomeNews(ListView): #возвращает список новостей, переопределяем аттрибуты, остальной рендер за нас делает класс
